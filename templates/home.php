c009acc4332e34cb5dcf7fc8253836f6e7e8a08f
Shiny Alex <carvantes.alex@gmail.com>
2018-06-19 17:18:09 -0500

    Home html

--- templates/home.php
+++ templates/home.php
@@ -3,7 +3,177 @@
     get_header();
 ?>
 
-section#
+<section id="banner-home">
+    
+</section>
+
+<section id="servicios">
+    <div class="contenedor">
+       <div class="center">
+             <h2>
+               NUESTROS
+            <span class="nv">
+                SERVICIOS
+            </span></h2>
+       </div>
+      
+        
+        <div class="flex">
+            <div class="col">
+                <h3>
+                CONTABILIDAD Y
+                <span class="bigger">
+                    ADMINISTRATIVOS
+                </span></h3>
+                
+                <div class="center">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/calculadora-lapiz.png" alt="contabilidad aplicada">
+                    <a href="#" class="btn-navy">¡Me interesa!</a>
+                </div>
+            </div>
+            <div class="col">
+                <ul>
+                    <li>Elaboración de Contabilidad Electrónica.</li>
+                    <li>Administración de la nómina.</li>
+                    <li>Elaboración de Estados Financieros.</li>
+                    <li>Determinación y presentación de pagos provisionales, pagos definitivos, declaraciones anuales, declaraciones informativas, etc.</li>
+                    <li>Determinación y presentación de Cuotas Obrero – Patronales ante el Instituto Mexicano del Seguro Social (IMSS). </li>
+                    <li>Presentación de avisos mediante los diferentes portales de las autoridades fiscales y administrativas, así como la solventación de notificaciones.</li>
+                    <li>Gestión ante las autoridades municipales para la obtención de la licencia de funcionamiento.
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="legales">
+    <div class="contenedor">
+        <div class="flex">
+            <div class="col">
+                <ul>
+                    <li>Juicios Familiares.</li>
+                    <li>Juicios Civiles.</li>
+                    <li>Juicios Mercantiles.</li>
+                    <li>Juicios Laborales.</li>
+                    <li>Juicios Contenciosos Administrativos.</li>
+                </ul>
+            </div>
+            <div class="col">
+                <div class="left">
+                    <h2>
+                    SERVICIOS
+                    <span class="bigger">
+                        LEGALES
+                    </span></h2>
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/balanza.png" alt="JUSTICIA">
+                    <a href="#" class="btn-navy">¡Me interesa!</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="arrendamiento">
+    <div class="contenedor">
+        <div class="center">
+            <h2 class="white">
+                SERVICIOS DE
+                <span class="bigger">
+                    ARRENDAMIENTO
+                </span>
+            </h2>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/goldCoins.png" alt="">
+            <p class="white">¿QUÉ ES?</p>
+            <p class="white">
+            Es un financiamiento a mediano o a largo plazo para renovar o adquirir activos fijos como equipo de transporte, maquinaria o industrial.</p>
+        </div>
+    </div>
+</section>
+
+<section id="tipos">
+    <div class="contenedor">
+        <h2>
+        OFRECEMOS 3 TIPOS
+            <span class="bigger nv">
+                de Arrendamiento
+            </span>
+        </h2>
+        <div class="flex">
+            <div class="col">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/dolar-monedas.png" alt="dinero">
+                <h3>Financiero</h3>
+                <p>Para la adquisición del activo con opción de compra pactada al vencimiento.</p>
+            </div>
+            <div class="col">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/arr-puro.png" alt="contrato">
+                <h3>Puro</h3>
+                <p>Para uso y goce del activo, con valor residual pactado o ampliación de contrato de renta.</p>
+            </div>
+            <div class="col">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/arr-back.png" alt="garantía certificada">
+                <h3>Sale and lease back</h3>
+                <p>Para obtener líquidez, usando los  equipos propios como garantía.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="caracteristicas">
+    <div class="contenedor">
+        <div class="flex">
+            <div class="col">
+                <h2 class="nv">Características:</h2>
+                <ul>
+                    <li class="triangle">Se puede registrar en los Estados financieros como Activo Fijo (financiero).
+                    </li>
+                    <li class="triangle">
+                    La renta es un gasto 100% deducible de impuestos (excepto de autos los cuáles están impuestos por la Ley de ISR).
+                   </li>
+                    <li class="triangle">Ayuda a la planeación de flujos de la empresa.</li>
+                    <li class="triangle">Disposición a través de pago a proveedor.</li>
+                    <li class="triangle">Puedes arrendar equipos, bienes, nuevos, usados, maquinaria o de importación.</li>
+                </ul>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/franklins.png" alt="dólares">
+            </div>
+            <div class="col">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/mercedez-truck.png" alt="Camioneta nueva mercedez">
+                <ul>
+                    <li class="triangle">Ayuda a conservar su capital de trabajo y sus otras fuentes de financiamiento.</li>
+                    <li class="triangle">Destino específico en base a las necesidades del cliente.</li>
+                    <li class="triangle">Importe de acuerdo a la capacidad de pago.</li>
+                    <li class="triangle">Disposición de hasta 5 años.</li>
+                    <li class="triangle">Plazo de acuerdo al tipo de bien y vida comercial.</li>
+                    <li class="triangle">Financiamiento del IVA.</li>
+                    <li class="triangle">En pesos o dólares de los Estados Unidos de Norteamérica.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="contacto">
+    <div class="contenedor">
+        <div class="flex">
+            <div class="col">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/CAM.png" alt="CAM, CONTADORES Y ABOGADOS de Morelos">
+                <p class="white"><b>Luis Ángel Hurtado Vázquez</b></p>
+                <div class="flex">
+                    <div class="icol"></div>
+                    <div class="bigcol">Particular: (777) 404 12 83</div>
+                </div>
+                <div class="flex">
+                    <div class="icol"></div>
+                    <div class="bigcol">Celular: (777) 258 60 25 / (777) 360 85 71</div>
+                </div>
+            </div>
+            <div class="col">
+                <h2 class="white nv">Contacto</h2>
+                
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php 
     get_footer();
